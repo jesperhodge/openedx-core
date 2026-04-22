d7869faaf181b4e397ef2166a4ed525175482105
Jesper Hodge <jhodge@outlook.de>
2026-04-22 18:40:26 -0400

    fix: lint

--- src/openedx_tagging/rest_api/v1/serializers.py
+++ src/openedx_tagging/rest_api/v1/serializers.py
@@ -222,8 +222,9 @@ def validate_tag_value(value, context, original_value=None):
     """
     Validates the incoming request early:
     - This tag is unique, not a duplicate. (The model does not validate this sufficiently.)
-    - There are no forbidden / reserved characters present. There is an additional model-side validation for this as well,
-    but we are keeping this so we can validate the incoming request immediately.
+    - There are no forbidden / reserved characters present. There is an additional
+    model-side validation for this as well, but we are keeping this so we can validate
+    the incoming request immediately.
     """
     taxonomy_id = context.get("taxonomy_id")
     original_tag = Tag.objects.filter(taxonomy_id=taxonomy_id, value=original_value).first() if original_value else None
